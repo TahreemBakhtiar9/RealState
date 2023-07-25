e6895e1ae3d1cdcdc2926d118926f871311e9447
TahreemBakhtiar9 <tehreembakhtiar927@gmail.com>
2023-07-25 10:59:14 +0500

    dummy data

---
+++ src/utils/Accordion.jsx
@@ -0,0 +1,24 @@
+import {HiShieldCheck} from "react-icons/hi";
+import {MdCancel, MdAnalytics} from "react-icons/md";
+
+const data = [
+    {
+        icon:<HiShieldCheck/>,
+        heading: "Best interest rates on the market",
+        detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
+    },
+
+    {
+        icon:<MdCancel/>,
+        heading: "Prevent unstable prices",
+        detail: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+
+    {
+        icon:<MdAnalytics/>,
+        heading: "Best Prices on the market.",
+        detail: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    },
+]
+
+export default data;
