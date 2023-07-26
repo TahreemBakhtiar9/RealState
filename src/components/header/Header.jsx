577b608990bd64e0746cc05475a5210cfa830430
TahreemBakhtiar9 <tehreembakhtiar927@gmail.com>
2023-07-26 16:45:25 +0500

    logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Header.css'
+import logo from "../../images/logo.png"
 
 const Header = () => {
   return (
     
     <section className='h-wrapper'>
         <div className='flexCenter paddings h-container'>
-<img src='../logo.png' alt='logo' width={100}/>
+<img src={logo} alt='logo' width={100}/>
 
 <div className='h-menu'>
     <a href=''> Residencies </a>
